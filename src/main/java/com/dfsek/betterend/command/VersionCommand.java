780eaf3773c204d3a803ea8d531161f979d0a31a
dfsek <dfsek@protonmail.com>
2020-11-15 16:48:47 -0700

    Add Gaea version to VersionCommand

--- src/main/java/com/dfsek/betterend/command/VersionCommand.java
+++ src/main/java/com/dfsek/betterend/command/VersionCommand.java
@@ -2,12 +2,15 @@ package com.dfsek.betterend.command;
 
 import com.dfsek.betterend.BetterEnd;
 import com.dfsek.betterend.config.LangUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.polydev.gaea.command.Command;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class VersionCommand extends Command {
     public VersionCommand(Command parent) {
@@ -26,7 +29,9 @@ public class VersionCommand extends Command {
 
     @Override
     public boolean execute(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        LangUtil.send("commands.version", sender, getMain().getDescription().getVersion() + "-" + (BetterEnd.isPremium() ? "premium" : "free"));
+        Plugin gaea = Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("Gaea"));
+        String gaeaVersion = gaea.getDescription().getVersion();
+        LangUtil.send("commands.version", sender, getMain().getDescription().getVersion() + "-" + (BetterEnd.isPremium() ? "premium" : "free"), gaeaVersion);
         return true;
     }
 
